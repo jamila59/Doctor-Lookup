2ded41fbd6183ff750a2af89cf6cafbcea8a9440
Jamila Aliyeva <padma@email.com>
2019-11-22 10:11:07 -0800

    pull info from api and will impliment a try catch for async function

--- src/doctor.js
+++ src/doctor.js
@@ -1,6 +1,11 @@
 export class Doctor {
   async getDoctor(){
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=john&query=heart&location=wa-seattle&skip=0&user_key=872f90d07969b706a5a39f922422b30a`);
+    try {
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=15&user_key=872f90d07969b706a5a39f922422b30a`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
+    } catch(error) {
+    console.error("There was an error handling your request: " + error.message);
     }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Doctor } from './doctor.js';
+import { Doctor } from './doctor';
 
 
 $(document).ready(function() {
-
+  let query = $("#doctorInput")
+  let doctor = new Doctor();
+  (async () => {
+    const response = await doctor.getDoctor();
+    let answerDoctor = getElements(response);
+    console.log(answerDoctor);
+  })();
+  const getElements = function(response) {
+    return response.data[0].practices[0].name;
+   };
 });
